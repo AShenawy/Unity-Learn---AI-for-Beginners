9d6a07ef4430eb25efdde4bb2b43ec8bc965a4a3
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-06-13 17:09:31 +0300

    Initial commit

---
+++ Assets/AI Maths/Vectors/Scripts/Move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.5f;
+
+    void Start()
+    {
+
+
+    }
+
+    private void Update()
+    {
+        transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/AI Maths/Vectors/Scripts/MoveToGoal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    public Transform goal;
+    public float speed = 2f;
+    public float accuracy = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(goal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionVector = goal.position - transform.position;
+        Debug.DrawRay(transform.position, directionVector, Color.red);
+        if (Input.GetKey(KeyCode.G))
+        {
+            if (directionVector.magnitude > accuracy)
+                transform.Translate(directionVector.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
