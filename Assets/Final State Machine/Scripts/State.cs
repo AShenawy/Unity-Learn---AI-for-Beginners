792a4d775141bcd5fe07b28f0640e385e00bc578
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-06-13 20:19:01 +0300

    Created State script and Idle script
    * State script as base class for all states in a state machine
    * Idle script is the first to inherit from State and acts as the first part of the state machine

---
+++ Assets/Final State Machine/Scripts/State.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class State
+{
+    public enum STATE { IDLE, PATROL, PURSUE, ATTACK, SLEEP };
+
+    public enum EVENT { ENTER, UPDATE, EXIT };
+
+    public STATE name;
+    protected EVENT stage;
+    protected GameObject npc;
+    protected NavMeshAgent agent;
+    protected Animator anim;
+    protected Transform player;
+    protected State nextState;
+
+    private float visDist = 10f;
+    private float visAngle = 30f;
+    private float shootDist = 7f;
+    
+    public State(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+    {
+        npc = _npc;
+        agent = _agent;
+        anim = _anim;
+        stage = EVENT.ENTER;
+        player = _player;
+    }
+
+    public virtual void Enter() { stage = EVENT.UPDATE; }
+
+    public virtual void Update() { stage = EVENT.UPDATE; }
+
+    public virtual void Exit() { stage = EVENT.EXIT; }
+
+    public State Process()
+    {
+        if(stage == EVENT.ENTER)
+            Enter();
+
+        if (stage == EVENT.UPDATE)
+            Update();
+
+        if (stage == EVENT.EXIT)
+        {
+            Exit();
+            return nextState;       // Since this state is exiting, we need the next state that will be called
+        }
+        
+        return this;    // If not exiting, return this state
+    }
+}
